e2297a3b93d1cf2b27e303e55c80bec18dcb692a
destiny <destinyomorhienrhien1993@gmail.com>
2024-07-04 18:10:10 +0100

    more changes

--- client_ip/views.py
+++ client_ip/views.py
@@ -2,19 +2,32 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-# import requests
+import requests
+from .utils import get_client_ip
+
 
 # Create your views here.
 
 class HelloView(APIView):
     def get(self, request):
         visitor_name = request.GET.get("visitor_name", "Mark")
-        client_ip = request.META.get("REMOTE_ADDR", "127.0.0.1")
-
+        client_ip = get_client_ip(request)
         location = "New York"
-        temperature = 11
+        # location = get_client_city(client_ip)
+        # client_ip = request.META.get("REMOTE_ADDR", "127.0.0.1")
+        
+        api_key = '4b3573d9f89682c6dd3d5348b085ae34'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}'
 
-        greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degress celsius in {location}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            weather_data = response.json()
+            temperature = weather_data['main']['temp']
+        except (requests.RequestException, KeyError) as e:
+            temperature = "unavailable"
+
+        greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
 
         data = {
             'client_ip': client_ip,
@@ -23,3 +36,4 @@ class HelloView(APIView):
         }
 
         return Response(data, status=status.HTTP_200_OK)
+    
